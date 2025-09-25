9d07376b0fd4286f9f92df26629189bafc4eb15b
Velja20 <maveljko2004@gmail.com>
2025-09-25 16:17:53 +0200

    Dodataa funkcija sabiranja

--- src/kalkulator.py
+++ src/kalkulator.py
@@ -1,1 +1,12 @@
 # Početna verzija kalkulatora
+def saberi(a, b):
+    return a + b
+
+def main():
+    print("Kalkulator")
+    a = float(input("Unesite prvi broj: "))
+    b = float(input("Unesite drugi broj: "))
+    print("Rezultat:", saberi(a, b))
+
+if __name__ == "__main__":
+    main()
